93df76e3fc177602ab21a9a2301aa919f1c68215
Janilson Varela <janilsonjvs@gmail.com>
2026-07-18 10:55:56 -0300

    feat> configuracao do servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('index');
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send("Server up")
+})
+
+app.listen(PORT, () => {
+    console.log(`server running in port ${PORT}`)
+})
